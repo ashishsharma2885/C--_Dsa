45b83387a98d973fad010ed94e98753f546e10e9
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-09-09 10:10:25 +0530

    Add Hero class implementation with health and level attributes
    - Created a Hero class with private health property and public level property.
    - Implemented methods to get and set health and level.
    - Added a print method to display health.
    - Demonstrated object creation and usage in main function.

---
+++ Lecture-42/class.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+
+class Hero{
+    // properties
+    private:
+    int health;
+    public:
+    char level;
+    void print(){
+        cout << health << endl;
+    }
+    int getHealth(){
+        return health;
+    }
+
+    char getLevel(){
+        return level;
+    }
+    void setHelth(int h){
+        health = h;
+    }
+
+    void setLevel(char ch){
+        level = ch;
+    }
+
+};
+
+int main(){
+
+    // creation of object
+    Hero Ashish;
+    cout << "Ashish health is " << Ashish.getHealth() << endl;
+    Ashish.setHelth(70);
+    // Ashish.health = 70;
+    Ashish.level = 'A';
+
+    cout << "health is: " << Ashish.getHealth()  << endl;
+    cout << "Level is: "  << Ashish.level << endl;
+
+    // cout << "size: " << sizeof(h1) << endl;
+
+}
