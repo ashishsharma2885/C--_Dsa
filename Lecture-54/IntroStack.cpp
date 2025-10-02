0bf9a9b2a7e10169c907663c6790d12bceec39b5
ashishsharma2885 <111562868+ashishsharma2885@users.noreply.github.com>
2025-10-02 10:36:32 +0530

    Implemented a custom stack in C++ with full functionality including push, pop, peek, and isEmpty methods. Added handling for stack overflow when trying to push beyond the defined size and stack underflow when attempting to pop from an empty stack. Updated the main function to demonstrate multiple push and pop operations, printing the top element after each change. Also included a friendly message output when the stack becomes empty, making the program more interactive and clear for demonstration purposes.

--- Lecture-54/IntroStack.cpp
+++ Lecture-54/IntroStack.cpp
@@ -2,7 +2,88 @@
 #include<stack>
 using namespace std;
 
+class Stack {
+    // properties
+   public: 
+   int *arr;
+   int top;
+   int size;
+
+   // behavior
+   Stack(int size){
+    this -> size = size;
+    arr = new int[size];
+    top = -1;
+   }
+
+   void push(int element){
+   if(size - top > 1){
+    top++;
+    arr[top] = element;
+   } else {
+     cout << "Stack OverFlow" << endl;
+   }
+ }
+
+   void pop() {
+    if(top >= 0){
+     top--;
+    } else {
+    cout << "Stack Underflow" << endl;
+    }
+   }
+
+   int peek(){
+     if(top >= 0)
+     return arr[top];
+     else {
+        cout << "Stack is empty" << endl;
+        return -1;
+     }
+   }
+
+   bool isEmpty(){
+   if(top == -1){
+    return true;
+   } else {
+    return false;
+   }
+   }
+
+};
+
+
 int main(){
+
+    Stack st(5);
+
+    st.push(22);
+    st.push(43);
+    st.push(44);
+    st.push(22);
+    st.push(43);
+    st.push(44);
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    if(st.isEmpty()) {
+        cout << "Stack is empty mere dost" << endl;
+    } else {
+        cout << "Stack is not empty mere dost" << endl;
+    }
+
+    /*
+
     // creation of stack
     stack<int> s;
 
@@ -20,4 +101,5 @@ int main(){
      cout << "Stack is not empty " << endl;
     }
     cout << "size of stack is " << s.size() << endl;
+    */
 }
